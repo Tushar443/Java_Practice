dbcba0a2df00dbbb2d65fcaac62c3308d364cac7
Tushar443 <moretushart33@gmail.com>
2020-06-15 22:32:37 +0530

    Hello world program

---
+++ Variables/src/variables.java
@@ -0,0 +1,25 @@
+
+public class variables {
+	public static void main(String[] args) {
+//		int x=10;
+//		System.out.println("Variable is "+x);
+	
+//		String s="Tushar";
+//		System.out.println("My name is "+s );
+		
+//		float a=(float) 2.0;
+//		float a=2.0f;
+//		int b=2;
+//		double c=2d;
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+		
+		byte a =2;
+		short b=6555;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+	}
+}
