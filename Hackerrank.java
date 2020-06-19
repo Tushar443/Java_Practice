6c2f6e992628f955aed1b2996cc109988d5914f4
Tushar443 <moretushart33@gmail.com>
2020-06-19 17:50:39 +0530

    New Commit 19 june

---
+++ Hackerrank.java
@@ -0,0 +1,18 @@
+// challenge 1
+
+System.out.println("Hello, World");
+System.out.println("Hello, Java");
+
+//challenge 2
+
+
+int b=scan.nextInt();
+System.out.println(b);
+int c=scan.nextInt();
+System.out.println(c);
+
+//challenge 3
+
+double d =scan.next();
+String s =
+
