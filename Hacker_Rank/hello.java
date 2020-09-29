b6e3a6a4f6954870348506df86327810b1666ef9
Tushar443 <moretushart33@gmail.com>
2020-09-29 09:39:33 +0530

    .

--- Hacker_Rank/hello.java
+++ Hacker_Rank/hello.java
@@ -1,6 +1,11 @@
-public class hello
-{
-    public static void main(String [] args){
-        System.out.println("Hello World");
-    }
+
+public class Hello {
+	public static void main(String [] args) {
+		for(int i=0 ;i <=3 ; i++) {
+			for(int j=0;j<=3;j++){
+				System.out.println(j);
+			}
+		
+		}
+	}
 }
